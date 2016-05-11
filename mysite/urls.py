3126530e10a050c48cc2b1d87276cef1d480e358
Mara Gatchalian <maragatchalian@KLabs-MacBook-Pro-6.local>
2016-05-11 16:40:26 +0800

    Edit posts

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from vanillablog.views import ListView, CreatePost, ViewPost, DeletePost
+from vanillablog.views import ListView, CreatePost, ViewPost, DeletePost, EditPost
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', ListView.as_view(), name='list'),
     url(r'^selete/$', CreatePost.as_view(), name='create_post'),
     url(r'^post/(?P<pk>\d+)/$', ViewPost.as_view(), name='post_detail'),
+     url(r'^edit/(?P<pk>\d+)/$', EditPost.as_view(), name='edit_post'),
     url(r'^delete/(?P<pk>\d+)/$', DeletePost.as_view(), name='delete_post'),
 ]
--- vanillablog/views.py
+++ vanillablog/views.py
@@ -1,11 +1,10 @@
-from vanilla import ListView, CreateView, DetailView, DeleteView
+from vanilla import ListView, CreateView, DetailView, DeleteView, UpdateView
 from vanillablog.forms import PostForm
 from django.utils import timezone
 from django.core.urlresolvers import reverse_lazy
 from django.template import RequestContext
 from .models import List
 from django.http import HttpResponseRedirect
-from django.core.exceptions import ImproperlyConfigured
 
 class ListView(ListView):
     model = List
@@ -39,12 +38,27 @@ class DeletePost(DeleteView):
     success_url = reverse_lazy('list')
     template_name_suffix = '_confirm_delete'
 
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        context = self.get_context_data()
-        return self.render_to_response(context)
-
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
         self.object.delete()
         return HttpResponseRedirect(self.get_success_url())
+
+class EditPost(UpdateView):
+    model = List
+    form_class = PostForm
+    success_url = reverse_lazy('list')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form(data=request.POST, files=request.FILES, instance=self.object)
+        if form.is_valid():
+            return self.form_valid(form)
+        return self.form_invalid(form)
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        context = self.get_context_data(form=form)
+        return self.render_to_response(context)
